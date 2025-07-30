5ba6d773be914f966ee11eb818ef513b3b363967
Kusillo <colectivoenrojo@gmail.com>
2025-07-30 10:30:15 -0400

    feat: enhance toolbar and job postings layout with responsive design and improved structure

--- src/app/SharedModule/views/job-postings/job-postings.component.ts
+++ src/app/SharedModule/views/job-postings/job-postings.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ToolbarComponent } from '../../toolbar/toolbar.component';
+import { LayoutComponent } from '../../layout/layout.component';
+import { AsyncPipe } from '@angular/common';
+import { SessionService } from '../../../services';
 
 @Component({
   selector: 'app-job-postings',
-  imports: [],
+  imports: [ToolbarComponent, LayoutComponent, AsyncPipe],
   templateUrl: './job-postings.component.html',
   styleUrl: './job-postings.component.scss'
 })
 export class JobPostingsComponent {
+  private sessionService = inject(SessionService);
+  
+    logout(): void {
+      this.sessionService.logout();
+    }
 
 }
